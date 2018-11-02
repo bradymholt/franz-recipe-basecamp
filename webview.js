23a12fb82758b5c22330d12928aa40eb6c84c201
Brady Holt <brady.holt@gmail.com>
2018-11-02 16:44:29 -0500

    first commit

---
+++ webview.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = (Franz, options) => {
+  const getMessages = () => {
+    const allMessages = document.querySelectorAll('.unread-badge')[1].getAttribute('data-badge-count');
+
+    // set Franz badge
+    Franz.setBadge(allMessages);
+  };
+
+
+  // check for new messages every second and update Franz badge
+  Franz.loop(getMessages);
+};
